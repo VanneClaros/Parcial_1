1e8205d41c0e8517175904bc57d8cc33a4615335
vane <vanessaclaros97@hotmail.com>
2025-08-02 18:57:59 -0400

    Create Marca

---
+++ backend/app/Http/Controllers/MarcaController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Marcas;
+use Illuminate\Http\Request;
+
+class MarcaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        return response()->json(Marcas::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+         //validación
+        $request->validate([
+            'nombre'=>'required',
+            'descripcion'=>'nullable'
+        ]);
+
+        $marcas = Marcas::create($request->all());
+
+        return response()->json([
+            'mensaje'=>'Marca creada exitosamente',
+            'marcas'=> $marcas
+        ],201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        // 
+        $marcas = Marcas::find($id);
+
+        if(!$marcas){
+
+            return response()->json(
+                [
+                    'mensaje'=>'Marca no encontrada'
+                ],404
+                );
+        }
+
+        return response()->json($marcas,200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MarcaController;
+
+Route::apiResource('marcas',MarcaController::class); 
 
 Route::post('/register',[AuthController::class,'register']);
+
 Route::post('/login',[AuthController::class,'login']);

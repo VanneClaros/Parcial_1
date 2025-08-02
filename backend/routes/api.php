eb10ea43b8c27dfaad7a140eff50bbb18f54ae25
vane <vanessaclaros97@hotmail.com>
2025-08-02 15:30:41 -0400

    Actualizacion

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,9 +1,7 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
 
 Route::post('/register',[AuthController::class,'register']);
-
 Route::post('/login',[AuthController::class,'login']);
